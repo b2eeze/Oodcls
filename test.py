755b5c1ad626ab9e9c810a40711daf07d1124a51
Zichuan-c <125120249+Zichuan-c@users.noreply.github.com>
2023-06-17 17:02:21 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,12 +2,13 @@ from oodcls import OodCls
 import torch
 
 # 创建 OodCls 实例
-ood_classifier = OodCls()
+
+ood_cls = OodCls()
 
 # 准备输入数据
-imgs = torch.randn(4, 1, 28, 28)
+imgs = torch.randn(5, 1, 28, 28)
+# 输入到GPU
+imgs = torch.tensor([item.cpu().detach().numpy() for item in imgs]).cuda()
 
 # 进行预测
-preds = ood_classifier.classify(imgs)
-
-print("Predict Label is:", [i.item() for i in preds.data])
+preds = ood_cls.classify(imgs)
