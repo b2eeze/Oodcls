039b5994a8da7f954ff64f5710ee7dc9e643f2c9
Zichuan-c <125120249+Zichuan-c@users.noreply.github.com>
2023-06-17 16:38:52 +0800

    Update test_2.py

--- test_2.py
+++ test_2.py
@@ -1,106 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-import torch
-from torchvision import datasets, transforms
-from torch.utils.data import ConcatDataset, DataLoader, TensorDataset
-import matplotlib.pyplot as plt
-import os
-import torchvision
-import numpy as np
-from torch.autograd import Variable
-
-import torch.nn.functional as F
-
-
-# In[2]:
-
-
-class Model(torch.nn.Module):
-    
-    def __init__(self):
-        super(Model, self).__init__()
-        
-        # 卷积层
-        self.conv1 = torch.nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, stride=1, padding=1)
-        self.conv2 = torch.nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
-        
-        # 全连接层
-        self.fc1 = torch.nn.Linear(128*7*7, 1024)
-        self.fc2 = torch.nn.Linear(1024, 11)
-        
-    def forward(self, x):
-        # 卷积层1
-        x = F.relu(self.conv1(x))
-        x = F.max_pool2d(x, 2)
-        
-        # 卷积层2，使用Dropout正则化
-        x = F.relu(self.conv2(x))
-        x = F.dropout(x, p=0.2)
-        x = F.max_pool2d(x, 2)
-
-        # 全连接层，使用L2正则化
-        x = x.view(-1, 128*7*7)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, p=0.5)
-        x = self.fc2(x)
-        return x
-
-
-# In[56]:
-
-
-class OodCls:
-    def __init__(self):
-       self.M = Model()
-       self.M = self.M.cuda()
-       self.M.load_state_dict(torch.load('model_parameter_ood.pkl')) 
-    
-    def misc(self, x):
-        # 转为灰度图像
-        x_gray = transforms.Grayscale(x)
-    
-    def classify(self, imgs : torch.Tensor) -> torch.Tensor:
-        
-        # 获取输入张量的维度
-        num_dims = imgs.size()[0]
-        
-        data_loader = torch.utils.data.DataLoader(dataset = imgs,
-                                          batch_size = num_dims,
-                                          shuffle = True)
-        x = next(iter(data_loader))
-        
-        # 图形优化
-        x_modify = self.misc(x)
-        
-        x_test = Variable(x)
-        preds = self.M(x_test)
-        _, preds = torch.max(preds, 1)
-        print("Predict Label is:", [ i.item() for i in preds.data])
-        
-        
-        img = torchvision.utils.make_grid(x)
-        img = img.cpu().numpy().transpose(1,2,0)
-
-        std = [0.5]
-        mean = [0.5]
-        img = img*std+mean
-        plt.imshow(img)
-        
-        return preds.data
-
-
-# In[57]:
-
+from oodcls import OodCls
 
 ood_cls = OodCls()
-
-
-# In[58]:
-
 
 # 构建含有OOD类别的train_data
 
@@ -132,10 +32,6 @@ test_cifar10.targets = torch.tensor(label_list2, dtype=torch.long)
 train_cifar10.targets = train_cifar10.targets.tolist()
 test_cifar10.targets = test_cifar10.targets.tolist()
 
-
-# In[59]:
-
-
 # 加载 KMNIST 数据集
 
 transform = transforms.Compose([
@@ -163,10 +59,6 @@ test_kmnist.targets = torch.tensor(label_list2, dtype=torch.long)
 # 检查数据类型是否与 mnist 一致
 # print(type(train_cifar10.targets))
 
-
-# In[60]:
-
-
 # 加载 fashionMNIST 数据集
 
 train_fmnist = datasets.FashionMNIST('./data/', train=True, download=True, transform=transform)
@@ -188,9 +80,6 @@ test_fmnist.targets = torch.tensor(label_list2, dtype=torch.long)
 # print(type(train_cifar10.targets))
 
 
-# In[61]:
-
-
 # 加载 MNIST 数据集
 
 train_mnist = datasets.MNIST(root='./data/', train=True, download=True, transform=transform)
@@ -198,9 +87,6 @@ test_mnist = datasets.MNIST(root="./data/", train = False, transform = transform
 
 # 检查数据类型
 # print(type(train_mnist.targets))
-
-
-# In[62]:
 
 
 # 合并数据集
@@ -213,13 +99,7 @@ test_data = ConcatDataset([test_cifar10, test_mnist, test_kmnist, test_fmnist])
 # 完成
 
 
-# In[63]:
-
-
 # print(test_data.target)
-
-
-# In[87]:
 
 
 # 导入准备好的数据
@@ -235,15 +115,7 @@ for i in range(100):
 imgs = torch.tensor([item.cpu().detach().numpy() for item in imgs]).cuda()
 
 
-
-# In[88]:
-
-
 print(imgs.shape)
-
-
-# In[89]:
-
 
 # 准备输入数据
 # imgs = torch.randn(5, 1, 28, 28)
@@ -255,9 +127,6 @@ preds = ood_cls.classify(imgs)
 # print(preds.shape)
 
 # print("Predict Label is:",  preds[2])
-
-
-# In[ ]:
 
 
 
